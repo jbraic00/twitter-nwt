4c3a160336d729de1c65879fc00d8e35af0ff337
Josipa Braica <josipa@dump.hr>
2016-08-24 15:51:15 +0200

    added user model

---
+++ twitter-nwt/Assets/app/Common/Models/Tweet.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=Tweet.js.map
---
+++ twitter-nwt/Assets/app/Common/Models/User.ts
@@ -0,0 +1,34 @@
+﻿import { Tweet } from './Tweet';
+import { Comment } from './Comment';
+
+export class User {
+    public ID: number;
+    public username: string;
+    public password: string;
+    public name: string;
+    public lastname: string;
+    public email: string;
+    public myTweets: Tweet[];
+    public favoritedTweets: Tweet[];
+    public followingUsers: User[];
+    public followedBy: User[];
+    public comments: Comment[];
+
+    constructor(ID: number = Math.random(), username: string, password: string, name: string, lastname: string, email: string, myTweets: Tweet[] = [], favoritedTweets: Tweet[] = [], followingUsers: Tweet[] = [], followedBy: Tweet[] = [], comments: Comment[] = []) {
+        this.ID = ID;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.myTweets = myTweets;
+        this.favoritedTweets = favoritedTweets;
+        this.followingUsers = followingUsers;
+        this.followedBy = followedBy;
+        this.comments = comments;
+    }
+
+    public getFullName(): string {
+        return this.name + " " + this.lastname;
+    }
+}
