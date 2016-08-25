587ca0f466f4d2e2d2edb752f1a118afd0200af1
Izabela Domazet <v-izabela.domazet@dump.hr>
2016-08-25 17:33:21 +0200

    added tweet service and tweet-list component

--- twitter-nwt/Assets/app/tweets/tweets-list.component.ts
+++ twitter-nwt/Assets/app/tweets/tweets-list.component.ts
@@ -1,8 +1,36 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { Observable }        from 'rxjs/Observable';
+import { TweetService } from './../Common/Services/TweetService';
+import { Tweet } from './../Common/Models/Tweet';
 
 @Component({
     selector: 'tweets-list',
-    template: '<h3>Tweets listttt</h3>'
+    template: `<h3>Tweets list:</h3>
+                <ul>
+                  <li *ngFor="let tweet of tweets">
+                    {{tweet.Text}}
+                  </li>
+                </ul>
+              `,
+    providers: [TweetService]
 })
 
-export class TweetsListComponent { }
+export class TweetsListComponent implements OnInit {
+    errorMessage: any;
+    tweets: Tweet[];
+
+    constructor(private tweetService: TweetService) { }
+
+    getTweets() {
+        this.tweetService.getTweets()
+            .subscribe(
+            tweets => { this.tweets = tweets; console.log(this.tweets) },
+            error => this.errorMessage = <any>error
+            );
+    } 
+
+    ngOnInit(): void {
+        this.getTweets();        
+    }
+
+}
