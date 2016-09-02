51d3cf841f35544b0f2a40b8dfe90998e3a38c7f
Josipa Braica <josipa@dump.hr>
2016-09-02 15:00:29 +0200

    added others' profile view

---
+++ twitter-nwt/Assets/app/others-profile/others-profile.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { User } from './../Common/Models/User';
+import { UserService } from './../Common/Services/UserService';
+
+@Component({
+    moduleId: module.id,
+    selector: 'others-profile',
+    templateUrl: 'others-profile.template.html'
+})
+
+export class OthersProfileComponent implements OnInit {
+    user: User;
+
+    constructor(private userService: UserService, private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.route.params.forEach((params: Params) => {
+            let id = +params['id'];
+            this.user = this.userService.getUserById(id);
+            console.log("Viewing users profile: ", this.user);
+        });
+    }
+}
